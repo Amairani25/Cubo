5faf42245d9291268b7570eb313922e27217041d
amair <amair@LAPTOP-Q9P3M1OQ>
2017-11-10 22:47:13 -0600

    Add project files.

---
+++ Cubo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cubo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa la medida de un lado de tu cubo");
+            int l = int.Parse(Console.ReadLine());
+            Console.WriteLine("El area de tu cubo es: A=6*("+ l +"^2)=" + (6 * (l*l)));
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
